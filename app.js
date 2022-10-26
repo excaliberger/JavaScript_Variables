36debefeefa695341cb60d99520bd94dfa533e59
Emily Berger <excaliberger@gmail.com>
2022-10-26 17:37:18 -0400

    Exercise 2 & 3

--- app.js
+++ app.js
@@ -4,10 +4,16 @@ console.log(
 );
 
 // Exercise 1
-const firstName = "Emily"
-let lastName = "Berger"
-var age = 29
+const firstName
+let lastName
+var age
 
 // Exercise 2
+const firstName = "Emily";
+let lastName = "Berger";
+var age = 29
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
